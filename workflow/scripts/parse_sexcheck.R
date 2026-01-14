8aff499deaa8ab25f1d080d49f218d4be2ad84c8
Jai Broome <jbroome@uw.edu>
2026-01-14 11:07:46 -0800

    fix parse_sexcheck output names

--- workflow/scripts/parse_sexcheck.R
+++ workflow/scripts/parse_sexcheck.R
@@ -7,10 +7,10 @@ pass <- dplyr::filter(sexcheck, STATUS == "OK")$IID
 fail <- dplyr::filter(sexcheck, STATUS == "PROBLEM")$IID
 
 if (length(fail) > 0) {
-    saveRDS(fail, output$fail_sexcheck)
+    saveRDS(fail, output$fail)
 } else {
     message("No samples with STATUS == 'PROBLEM' found in sexcheck results.")
 }
 
-writeLines(table(sexcheck$STATUS, useNA = "ifany"), output$sexcheck_summary)
-saveRDS(pass, output$pass_sexcheck)
+writeLines(table(sexcheck$STATUS, useNA = "ifany"), output$summary)
+saveRDS(pass, output$pass)
