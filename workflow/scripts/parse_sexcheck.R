2aafd0ff55668976fc484a5a754e16e49be24266
Jai Broome <jbroome@uw.edu>
2026-01-14 11:16:58 -0800

    write empty fail vector if there are no sexcheck fails

--- workflow/scripts/parse_sexcheck.R
+++ workflow/scripts/parse_sexcheck.R
@@ -6,11 +6,10 @@ sexcheck <- read.table(input[[1]], header = TRUE, stringsAsFactors = FALSE)
 pass <- dplyr::filter(sexcheck, STATUS == "OK")$IID
 fail <- dplyr::filter(sexcheck, STATUS == "PROBLEM")$IID
 
-if (length(fail) > 0) {
-    saveRDS(fail, output$fail)
-} else {
+if (length(fail) == 0) {
     message("No samples with STATUS == 'PROBLEM' found in sexcheck results.")
 }
 
+saveRDS(fail, output$fail)
 writeLines(table(sexcheck$STATUS, useNA = "ifany"), output$summary)
 saveRDS(pass, output$pass)
