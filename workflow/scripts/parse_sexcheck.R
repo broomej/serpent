e3ccb5777f5170133bf3f4796793eb023758df8c
Jai Broome <jbroome@uw.edu>
2026-01-14 10:58:12 -0800

    add plink sexcheck parser

---
+++ workflow/scripts/parse_sexcheck.R
@@ -0,0 +1,17 @@
+input <- snakemake@input
+output <- snakemake@output
+params <- snakemake@params
+
+sexcheck <- read.table(input[[1]], header = TRUE, stringsAsFactors = FALSE)
+
+pass <- dplyr::filter(sexcheck, STATUS == "OK")$IID
+fail <- dplyr::filter(sexcheck, STATUS == "PROBLEM")$IID
+
+if (length(fail) > 0) {
+    saveRDS(fail, output$fail_sexcheck)
+} else {
+    message("No samples with STATUS == 'PROBLEM' found in sexcheck results.")
+}
+
+writeLines(table(sexcheck$STATUS, useNA = "ifany"), output$sexcheck_summary)
+saveRDS(pass, output$pass_sexcheck)
