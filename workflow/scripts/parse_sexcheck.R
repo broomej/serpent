882db1d8f7096b6c3b2cd696f29d5d43a9790095
Jai Broome <jbroome@uw.edu>
2026-02-11 12:56:11 -0800

    add plink check-sex

--- workflow/scripts/parse_sexcheck.R
+++ workflow/scripts/parse_sexcheck.R
@@ -1,15 +1,17 @@
 input <- snakemake@input
 output <- snakemake@output
+library(dplyr)
 
 sexcheck <- read.table(input[[1]], header = TRUE, stringsAsFactors = FALSE)
 
-pass <- dplyr::filter(sexcheck, STATUS == "OK")$IID
-fail <- dplyr::filter(sexcheck, STATUS == "PROBLEM")$IID
+pass <- filter(sexcheck, STATUS == "OK")$IID
+fail <- filter(sexcheck, STATUS == "PROBLEM")$IID
 
 if (length(fail) == 0) {
     message("No samples with STATUS == 'PROBLEM' found in sexcheck results.")
 }
 
 saveRDS(fail, output$fail)
-writeLines(table(sexcheck$STATUS, useNA = "ifany"), output$summary)
 saveRDS(pass, output$ok)
+count(sexcheck, STATUS) %>%
+    write.table(output$summary, quote = FALSE, sep = "\t", row.names = FALSE)
