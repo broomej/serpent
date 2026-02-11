947d09f2750a9453a7e42acc8ab6a539d87cd3cd
Jai Broome <jbroome@uw.edu>
2026-02-10 21:59:14 -0800

    add king fixes and snakemake rule

--- workflow/scripts/king.R
+++ workflow/scripts/king.R
@@ -1,21 +1,41 @@
-#! /usr/bin/env Rscript
+input <- snakemake@input
+output <- snakemake@output
+params <- snakemake@params
+names(params) <- gsub("_", ".", names(params))
 
 library(ggplot2)
 library(SeqArray)
-gds <- seqOpen(snakemake@input$gds)
-if ("variant_id" %in% names(input)) {
-    seqSetFilter(gds, variant.id = readRDS(input$variant_id))
-}
-if ("sample_id" %in% names(input)) {
-    seqSetFilter(gds, sample.id = readRDS(input$sample_id))
+
+gds <- seqOpen(input$gds_fn)
+
+if ("seed" %in% names(params)) {
+    set.seed(params[["seed"]])
+    params[["seed"]] <- NULL
 }
 
-king <- SNPRelate::snpgdsIBDKING(
-    gds,
-    snp.id = readRDS(snakemake@input$var),
-    type = "KING-robust",
-    num.thread = as.integer(snakemake@threads)
+arguments <- list(
+    gdsobj = gds,
+    num.thread = as.numeric(snakemake@threads)
 )
+
+if (length(params) > 0) {
+    for (n in names(params)) {
+        arguments[[n]] <- params[[n]]
+    }
+}
+
+if ("snp_id" %in% names(input)) {
+    arguments[["snp.id"]] <- readRDS(input$snp_id)
+}
+if ("sample_id" %in% names(input)) {
+    arguments[["sample.id"]] <- readRDS(input$sample_id)
+}
+if ("family_id" %in% names(input)) {
+    arguments[["family.id"]] <- readRDS(input$family_id)
+}
+
+king <- do.call(SNPRelate::snpgdsIBDKING, arguments)
+print(str(king))
 
 rownames(king$kinship) <- king$sample.id
 colnames(king$kinship) <- king$sample.id
