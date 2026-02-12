533aef7aa250163dcca074b8611e97c26e5e24f2
Jai Broome <jbroome@uw.edu>
2026-02-11 16:47:48 -0800

    rework pcrelate to better handle snakemake inputs/params

--- workflow/scripts/pcrelate.R
+++ workflow/scripts/pcrelate.R
@@ -7,23 +7,42 @@ input <- snakemake@input
 output <- snakemake@output
 params <- snakemake@params
 
+n_pcs <- params$n_pcs
+params$n_pcs <- NULL
+vb <- params$variantBlock
+params$variantBlock <- NULL
+sk <- params$scaleKin
+params$scaleKin <- NULL
+
+pca <- readRDS(input$pca)
+
 gds <- seqOpen(input$gds)
-mypcair <- readRDS(input$pca)
-# replace following line with args to snpgdsIBDKING to read in sample/variant filters if provided
-seqSetFilter(gds, variant.id = readRDS(input$var))
+if (!is.null(input$variant_id)) {
+    seqSetFilter(gds, variant.id = readRDS(input$variant_id))
+}
 seqData <- SeqVarData(gds)
-iterator <- SeqVarBlockIterator(seqData, verbose = TRUE, variantBlock = 5000)
-pcr <- pcrelate(
-    iterator,
-    pcs = mypcair$vectors[, seq(params$n_pcs)],
-    training.set = mypcair$unrels,
-    small.samp.correct = TRUE
+iterator <- SeqVarBlockIterator(seqData, verbose = params$verbose,
+                                variantBlock = vb)
+
+arguments <- list(
+    gdsobj = iterator,
+    pcs = pca$vectors[, 1:n_pcs],
+    training.set = pca$unrels
 )
 
+if (!is.null(input$sample_include)) {
+    arguments$sample.include <- readRDS(input$sample_include)
+}
+
+if (length(params) > 0) {
+    for (n in names(params)) {
+        arguments[[n]] <- params[[n]]
+    }
+}
+names(arguments) <- gsub("_", ".", names(arguments))
+
+pcr <- do.call(pcrelate, arguments)
 saveRDS(pcr, output$pcr)
-pcr_mat <- pcrelateToMatrix(
-    pcr,
-    scaleKin = params$scale_kin
-)
 
+pcr_mat <- pcrelateToMatrix(pcr, scaleKin = sk)
 saveRDS(pcr_mat, output$grm)
